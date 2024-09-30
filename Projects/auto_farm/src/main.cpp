b365938d521fa681e75023e7d3ffb4046135e349
MyLastNameCk <111222909+MyLastNameCk@users.noreply.github.com>
2024-09-30 21:17:41 +0800

    NEW
    hai

---
+++ Projects/auto_farm/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+
+#define buzzer_pin 18
+
+const int sensor_soil = 2;
+const int sensor_water = 32;
+const int led_merah = 21;
+const int led_hijau = 19;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Project autofarm by deva");
+  pinMode(buzzer_pin, OUTPUT);
+  pinMode(led_merah, OUTPUT);
+  pinMode(led_hijau, OUTPUT);
+
+  digitalWrite(led_merah, LOW);
+  digitalWrite(buzzer_pin, LOW);
+  digitalWrite(led_hijau, LOW);
+} 
+
+void loop() {
+  int nilaisensor_soil_moisure = analogRead(sensor_soil);
+  int nilaisensor_water = analogRead(sensor_water);
+
+  float kelembapan = map(nilaisensor_soil_moisure, 0, 4095, 0, 100);
+  kelembapan = constrain(kelembapan, 0, 100);
+
+  Serial.printf("Nilai Soil Moisure : %d\n", nilaisensor_soil_moisure);
+  Serial.printf("Persentase Kelembapan : %d %%\n", kelembapan);
+  Serial.printf("Nilai Water Sensor : %d\n", nilaisensor_water);
+
+  delay(1000);
+
+  if (nilaisensor_soil_moisure < 50 and nilaisensor_water > 300) {
+    digitalWrite(led_merah, HIGH);
+    digitalWrite(buzzer_pin, HIGH);
+    Serial.println("Harus Disiram Bang");
+  } else {
+    digitalWrite(led_hijau, HIGH);
+  }
+}
